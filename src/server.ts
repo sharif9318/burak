08ecf1e9bd21ebe6ef7e14bbe28bcc18e1465adc
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-21 23:06:43 +0900

    patterns:short noted comments

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
-console.log("is it working?");
+// console.log("is it working?");
 
-// const person: string = "Toshmat";
+// // const person: string = "Toshmat";
+// // const count: number = 100;
+
+// import moment from "moment";
+
+// const currentTime = moment().format("YYYY:MM:DD: HH:mm:ss");
+// console.log(currentTime);
+
+// const person: string = "Martin";
 // const count: number = 100;
 
-import moment from "moment";
+//architectural pattern:
+// (backend) MVC (Model-View-Controller), DI (Dependency Injection),
+// (frontend) MVP (model-view-presenter), MVVM (model-view-view-model),
 
-const currentTime = moment().format("YYYY:MM:DD: HH:mm:ss");
-console.log(currentTime);
-
-const person: string = "Martin";
-const count: number = 100;
+//Design Patterns: middleware, decorators, etc.
