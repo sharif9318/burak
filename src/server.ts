255567a48670c8bb31288cd94f0ebc306606a81c
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-22 03:23:27 +0900

    feat:connect MongoDB via Mongoose

--- src/server.ts
+++ src/server.ts
@@ -19,3 +19,14 @@
 
 import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    const PORT = process.env.PORT ?? 3003;
+    console.log(`MongoDB connected successfully: ${PORT}`);
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
