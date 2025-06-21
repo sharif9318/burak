734a369d401a7487d1546eb39366c81bda1ef20d
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-21 12:05:59 +0900

    initial setup

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+console.log("is it working?");
+
+// const person: string = "Toshmat";
+// const count: number = 100;
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY:MM:DD: HH:mm:ss");
+console.log(currentTime);
+
+const person: string = "Martin";
+const count: number = 100;
