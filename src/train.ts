27b46eca11d81d5e4e1b9c79c4078c117dc09fe7
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-23 09:39:55 +0900

    fear:task H

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,7 @@
+import { stringify } from "querystring";
+
+console.log("=======task_g==========");
+
 function getHighestIndex(arr: number[]): number {
   let max = arr[0];
   let index = 0;
@@ -15,4 +19,21 @@ console.log(
   getHighestIndex([
     9, 4, 94, 6, 36, 2, 1, 0, 3, 5, 7, 8, 10, 11, 12, 13, 14, 15,
   ])
+);
+
+console.log("=======task_h==========");
+
+function getPositiveAsString(arr: number[]): string {
+  let result = "";
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      result += arr[i].toString();
+    }
+  }
+  return result;
+}
+
+console.log(
+  getPositiveAsString([9, -4, 94, -6, 36, 2, -1, 0, 3, 5, -7, 8, 10, 11, 12])
 );
