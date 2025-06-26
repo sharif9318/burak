dbe82d9a86f3c3d53b40d6d9d6dceda9aac178cc
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-26 18:46:50 +0900

    feat:task_h2

--- src/train.ts
+++ src/train.ts
@@ -37,3 +37,16 @@ function getPositiveAsString(arr: number[]): string {
 console.log(
   getPositiveAsString([9, -4, 94, -6, 36, 2, -1, 0, 3, 5, -7, 8, 10, 11, 12])
 );
+
+console.log("=======task_h2==========");
+
+function getDigits(input: string): string {
+  return input.replace(/\D/g, "");
+}
+//  \D means any character that is not a digit. (Digits are 0 through 9.)
+// - The g flag stands for global, which means "replace all matches in the string," not just the first one.
+// - '' is an empty string, which is what we're replacing the non-digit characters with.
+// The slashes /.../ are how you define a regular expression (also called a “regex”) in JavaScript and TypeScript.
+// They are like quotation marks, but for regular expressions.
+
+console.log(getDigits("a1b2c3d4e5f6g7h8i9j0"));
