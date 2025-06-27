88d5f7e6c05fde14a2c09faef7cc5aefabf8051e
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-06-27 19:43:01 +0900

    feat:installed morgan package and checked whetere it's working

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express from "express";
 import path from "path";
 import router from "./router";
 import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 // 1.ENTRANCE
 
-const app = express();
 // Middleware
+
+const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan(MORGAN_FORMAT)); // Logging middleware
 
 // 2.Sessions
 
