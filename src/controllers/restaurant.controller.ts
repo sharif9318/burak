05a25673ca6364009d7d4cbf716a20a3d34575e8
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-05 23:52:16 +0900

    fix: organize spa & ssr business logic

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -4,6 +4,8 @@ import MemberService from "../models/MemberService";
 import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member-enum";
 
+const memberService = new MemberService();
+
 const restaurantController: T = {};
 
 restaurantController.goHome = (req: Request, res: Response) => {
@@ -35,20 +37,6 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
-  try {
-    console.log("processLogin");
-    console.log("body:", req.body);
-    const input: LoginInput = req.body;
-    const memberService = new MemberService();
-    const result = await memberService.processLogin(input);
-    res.send(result);
-  } catch (err: any) {
-    console.log("Error, processLogin", err);
-    res.send(err);
-  }
-};
-
 restaurantController.processSignup = async (req: Request, res: Response) => {
   try {
     console.log("processSignup");
@@ -57,11 +45,28 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.RESTAURANT;
 
-    const memberService = new MemberService();
     const result = await memberService.processSignup(newMember);
+    //TODO:Sessions
+
     res.send(result);
   } catch (err) {
     console.log("Error, processSignup", err);
+    res.send(err);
+  }
+};
+
+restaurantController.processLogin = async (req: Request, res: Response) => {
+  try {
+    console.log("processLogin");
+    console.log("body:", req.body);
+    const input: LoginInput = req.body;
+
+    const result = await memberService.processLogin(input);
+    //TODO:Sessions
+
+    res.send(result);
+  } catch (err: any) {
+    console.log("Error, processLogin", err);
     res.send(err);
   }
 };
