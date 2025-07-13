75db32c6a5362921e4acbcb2c3d21a9878346aff
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-13 16:55:36 +0900

    fix: modify updateChosenProduct business logic

--- src/controllers/product-controller.ts
+++ src/controllers/product-controller.ts
@@ -54,6 +54,12 @@ await productService.createNewProduct(data);
 productController.updateChosenProduct = async (req: Request, res: Response) => {
   try {
     console.log("updateChosenProduct");
+const id = req.params.id;
+console.log("id:", id);
+const result = await productService.updateChosenProduct(id,req.body);
+
+
+res.status(HttpCode.OK).json({data: result });
   } catch (err) {
     console.log("Error, updateChosenProduct:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
--- src/models/product-service.ts
+++ src/models/product-service.ts
@@ -1,5 +1,6 @@
+import { shapeIntoMongooseObjectId } from "../libs/config";
 import Errors, { HttpCode, Message } from "../libs/errors";
-import { Product, ProductInput } from "../libs/types/product";
+import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/product-model";
 
 class ProductService {
@@ -20,6 +21,21 @@ public async createNewProduct(input: ProductInput): Promise<Product> {
     throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
   }
 }
+public async updateChosenProduct(
+    id: string,
+    input: ProductUpdateInput
+): Promise<Product> {
+    id = shapeIntoMongooseObjectId(id);
+    const result = await this.ProductModel
+        .findOneAndUpdate({ _id: id }, input, { new: true })
+        .exec();
+    if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+
+    // console.log("result:", result);
+    return result;
+}
+
+
 
 }
 
