c3ed0a4fab80f3db0215b7f198cc60f6dfa172be
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-20 08:23:07 +0900

    fix: admin project refactoring

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -24,7 +24,7 @@ $("#cancel-btn").on("click", () => {
 $(".new-product-status").on("change", async function (e) {
     const id = e.target.id;
     const productStatus = $(`#${id}.new-product-status`).val();
-    console.log("id:", id);
+
     console.log("productStatus:", productStatus);
 
     try {
@@ -34,7 +34,7 @@ $(".new-product-status").on("change", async function (e) {
         console.log("response:", response);
         const result = response.data;
         if (result.data) {
-            console.log("Product updated!");
+     
             $(`.new-product-status`).blur();
         } else alert("Product update failed!");
     } catch (err) {
--- src/public/js/users.js
+++ src/public/js/users.js
@@ -3,9 +3,7 @@ console.log("Users frontend javascript file");
 $(function () {
     $(".member-status").on("change", function (e) {
         const id = e.target.id;
-        console.log("id:", id);
-
-        const memberStatus = $(`#${id}.member-status`).val();
+            const memberStatus = $(`#${id}.member-status`).val();
         console.log("memberStatus:", memberStatus);
 
         axios
@@ -16,8 +14,7 @@ $(function () {
             .then((response) => {
                 console.log("response:", response);
                 const result = response.data;
-                console.log("result:", result);
-
+          
                 if (result.data) {
                     console.log("User updated!");
                     $(".member-status").blur();
