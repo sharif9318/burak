fc2b63d85e07d04adfeb8e50e96cc26c1305f0b6
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-07-25 11:04:52 +0900

    feat:task_r

--- src/train.ts
+++ src/train.ts
@@ -181,11 +181,24 @@
 // }
 // console.log(objectToArray({ 성함: "강건일", age: 30, city: "수원" }));
 
-console.log("=======task_q============")
+// console.log("=======task_q============")
 
-function hasProperty(obj: any, key: string): boolean {
-    return key in obj;
+// function hasProperty(obj: any, key: string): boolean {
+//     return key in obj;
+// }
+
+// console.log(hasProperty({ davlat: "Uzbekistan", viloyat: "Buxoro" }, "viloyat")); 
+// console.log(hasProperty({ davlat: "Uzbekistan", viloyat: "Buxoro" }, "tuman"));
+
+console.log("=======task_r============");
+
+function calculate(expression: string): number {
+  
+  const numbers = expression.split('+'); 
+  const sum = numbers.reduce((acc, val) => acc + parseInt(val.trim()), 0);
+
+  return sum;
 }
 
-console.log(hasProperty({ davlat: "Uzbekistan", viloyat: "Buxoro" }, "viloyat")); 
-console.log(hasProperty({ davlat: "Uzbekistan", viloyat: "Buxoro" }, "tuman"));
+
+console.log(calculate("35 + 986")); 
