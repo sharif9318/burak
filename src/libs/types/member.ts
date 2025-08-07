264c2c5c4aa4785dd79682bec7b8f07d640761ff
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-08-07 22:58:41 +0900

    feat: create middleware methods and develop logout process

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -45,6 +45,12 @@ export interface MemberUpdateInput {
   memberImage?: string;
 }
 
+export interface ExtendedRequest extends Request {
+  member: Member;
+  file: Express.Multer.File;
+  files: Express.Multer.File[];
+}
+
 
 export interface AdminRequest extends Request {
   member: Member;
