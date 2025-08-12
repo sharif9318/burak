5352e66bb87c69302f1408501412ea832921651f
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-08-12 18:36:13 +0900

    feat:create getRestaurant rest API

--- src/models/MemberService.ts
+++ src/models/MemberService.ts
@@ -5,15 +5,23 @@ import { MemberStatus, MemberType } from "../libs/enums/member-enum";
 import * as bcrypt from "bcryptjs";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 
-class MemberService {
-  // getMemberDetail(member: Member) {
-  //   throw new Error("Method not implemented.");
-  // }
+class MemberService { 
   private readonly memberModel;
   constructor() {
     this.memberModel = MemberModel;
   }
   /** SPA **/
+
+public async getRestaurant(): Promise<Member> {
+  const result = await this.memberModel
+    .findOne({ memberType: MemberType.RESTAURANT })
+    .lean()
+    .exec();
+  if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+  return result;
+}
+
+
   public async signup(input: MemberInput): Promise<Member> {
     const salt = await bcrypt.genSalt();
     input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
