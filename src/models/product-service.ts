2bd7793d32eb5f1459940ee720d9b10acd255c91
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-08-14 19:39:27 +0900

    feat: develop getProduct rest API

--- src/models/product-service.ts
+++ src/models/product-service.ts
@@ -4,6 +4,7 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { Product, ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/product-model";
 import { T } from "libs/types/common";
+import { ObjectId } from "mongoose";
 
 class ProductService {
   private readonly ProductModel;
@@ -39,6 +40,25 @@ public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
     .exec();
     
   if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+  return result;
+}
+
+public async getProduct(
+  memberId: ObjectId | null,
+  id: string
+): Promise<Product> {
+  const productId = shapeIntoMongooseObjectId(id);
+
+  let result = await this.ProductModel
+    .findOne({
+      _id: productId,
+      productStatus: ProductStatus.PROCESS,
+    })
+    .exec();
+  if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+  // TODO: If authenticated users => first => view log creation
 
   return result;
 }
